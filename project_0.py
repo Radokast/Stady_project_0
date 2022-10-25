2932246faba5f2a325958671825ed199c843997d
Radokast <radokast@gmail.com>
2022-10-25 15:05:43 +1000

    readmy
    Обновлён readmy по предлагаему образцу

---
+++ project_0.py
@@ -0,0 +1,63 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    max_number = 101
+    min_number = 1
+
+    while count < 100:
+        count += 1
+        predict_number = round((min_number + max_number) / 2)
+        print(f'загадано новое число ({number}), попытка угадать {count}, предполагаемое число {predict_number}')  
+        # загадываем предполагаемое число из округлённого значения среднего
+        
+        if number == predict_number:
+            break  # выход из цикла при верном варианте
+            
+        elif number < predict_number:
+            max_number = predict_number
+            
+        else: 
+            min_number = predict_number
+        
+    return count
+
+
+print(f'загадано новое число, попытка, число') 
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
